aab9612793f9a975ae9a79ad2046056121edbc48
chengivan01 <chengoleivan@gmail.com>
2022-09-29 19:04:21 +0300

    First commit. V1.0

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import ReactMarkdown from "react-markdown";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <PageComponent />
     </div>
   );
 }
 
-export default App;
+const PageComponent = () => {
+  const [content, setContent] = useState("");
+
+  useEffect(() => {
+    fetch("App.md")
+      .then((res) => res.text())
+      .then((text) => setContent(text));
+  }, []);
+
+  return (
+    <div className="post">
+      <ReactMarkdown children={content} />
+    </div>
+  );
+};
